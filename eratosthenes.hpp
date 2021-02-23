5e59f1d9dfa9e0303b46f3c317bdfdc27b19c050
void-hoge <coilgunrailgun@gmail.com>
2021-02-23 09:59:30 +0900

    namespaceを作った。結果を格納して扱うためのマネージャクラスを宣言した。

--- eratosthenes.hpp
+++ eratosthenes.hpp
@@ -3,7 +3,25 @@
 
 #include <ostream>
 #include <vector>
+#include <utility>
 
-void eratosthenes_binary_array(const long long limit, const int base_sieve_size, std::vector<std::vector<bool>>& data, std::ostream& debug);
+namespace voidhoge{
+
+class prime_binary_array {
+private:
+	std::vector<std::vector<bool>> data;
+	std::vector<int> base;
+	int sieve_max;
+public:
+	prime_binary_array () {};
+	prime_binary_array (const std::vector<std::vector<bool>>& data, const std::vector<int>& base, int sieve_max);
+	std::pair<long long, bool> at(const size_t base_pos, const size_t line_pos) const;
+	size_t get_base_size() const;
+	size_t get_line_size() const;
+};
+
+void eratosthenes_multithread(const long long limit, const int base_sieve_size, prime_binary_array& result, std::ostream& debug);
+
+} //namespace voidhoge
 
 #endif
